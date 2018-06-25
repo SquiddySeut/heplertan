0b776dd15b14bd5737bf30a486486f1e8d6eb18c
SquiddySeut <40527468+SquiddySeut@users.noreply.github.com>
2018-06-25 18:26:51 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,40 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var x = 0;
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+    //if (message.content === 'issei') {
+    	//message.reply('if you do not tell me where Issei is, I will murder you');
+  	//}
+    if (message.content === 'oppai') {
+        message.reply('you p-p-pervert!');
+    }
+    if (message.content === '~\hug') {
+        message.reply('aww, here is a hug for you!');
+    }
+    if (message.content === '~\lewdme') {
+        message.reply('no.');
+        message.reply('absolutely no');
+    }
+    if (message.content === '~\help') {
+        message.reply('my current commands are ~R/hug and ~R/help, sorry for so little commands');
+    }
+    if (message.content === '~\displayX') {
+        message.reply('x is currently '+ x);
+        //x++1;
+    }
+    if (message.content === '~\Xreset') {
+        message.reply('reseting X!');
+        //x==0;
+    }
+    if (message.content.startsWith("im")) {
+    message.channel.send("Hey Dad-bot, please shut up!");
+    
 });
 
 // THIS  MUST  BE  THIS  WAY
