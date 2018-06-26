098a1b3aaa04cd23ef2fff16d570c2ec8c50851f
SquiddySeut <40527468+SquiddySeut@users.noreply.github.com>
2018-06-26 18:34:34 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,11 +2,13 @@
 const Discord = require('discord.js');
 var x = 0; // new
 var stage0 = "You wake up in a lush forest. It's in the middle of the night and you notice a light somewhere in the distance to your right.";
-var quest0 = "Do you go Left to go deeper into the dark forest or Right towards the light?";
-var stage0l="You head deeper into the woods. You can't see anything, and suddenly, you have a searing pain in your chest.";
+var quest0 = "Do you go (left) to go deeper into the dark forest or (right) towards the light?";
+var stage0L="You head deeper into the woods. You can't see anything, and suddenly, you have a searing pain in your chest.";
 var gameover="You fall down, writhing in pain. You have died";
 var stage01="You find an abandoned warehouse, with a bright yellow light beaming from within";
-var quest01="Do you go inside, or wait outside?";
+var quest01="Do you (go inside), or (wait) outside?";
+var stage01L="You walk inside, quietly. You hear some footsteps behind you and you feel a blunt blow to the back of your head.";
+var stage02="You wait outside and hear some sort of screaming from the inside. Shortly after, you see a man in a cloak run out.";
 const client = new Discord.Client();
 
 
@@ -63,8 +65,18 @@ client.on('message', message => {
          x++;
    }
    if (message.content==="left"&&x===0){
-      message.reply(stage0l);
+      message.reply(stage0L);
       message.reply(gameover);
+   }
+   if (message.content==="wait"&&x===1){//stage01 results
+         message.reply(stage02);
+         message.reply(quest01);
+         x++;
+   }
+   if (message.content==="go inside"&&x===1){
+         message.reply(stage01L);
+         message.reply(gameover);
+         x=0;
    }
    
    
