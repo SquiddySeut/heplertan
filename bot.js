6e632af9c7ef28f3d6ac81cbb4f089f0b8e4890e
SquiddySeut <40527468+SquiddySeut@users.noreply.github.com>
2018-06-26 17:23:52 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,11 @@
 const Discord = require('discord.js');
 var x = 0; // new
 var stage0 = "You wake up in a lush forest. It's in the middle of the night and you notice a light somewhere in the distance to your right.";
-var quest0 = "Do you go Left or Right?";
-
-
+var quest0 = "Do you go Left to go deeper into the dark forest or Right towards the light?";
+var stage0l="You head deeper into the woods. You can't see anything, and suddenly, you have a searing pain in your chest.";
+var gameover="You fall down, writhing in pain. You have died";
+var stage01="You find an abandoned warehouse, with a bright yellow light beaming from within";
+var quest01="Do you go inside, or wait outside?";
 const client = new Discord.Client();
 
 
@@ -37,14 +39,39 @@ client.on('message', message => {
 
 
  	}
-   if (message.content==='gameStart'){
+   if (message.content==='gameStart'){//game startup
       if (x==0){
          message.reply(stage0);
          message.reply(quest0);
       }
+      if (x==01){
+         message.reply(stage01);
+         message.reply(quest01);
+      }
+      if (x==02){
+         message.reply(stage02);
+         message.reply(quest02);
+      }
+      if (x==03){
+         message.reply(stage03);
+         message.reply(quest03);
+      }
+   }
+   if (message.content===("right"&&x===0)){//stage0 results
+         message.reply(stage01);
+         message.reply(quest01);
+         x++;
+   }else{
+      message.reply(stage0l);
+      message.reply(gameover);
    }
    
    
+         
+         
+         
+         
+         
    
    
    
