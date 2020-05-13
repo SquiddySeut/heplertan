c0ea36848fc17e784c00e2aa02663fb265cad1f0
SquiddySeut <40527468+SquiddySeut@users.noreply.github.com>
2020-05-12 20:45:37 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,6 +14,14 @@ client.on('message', message => {
         message.channel.send('*I know how you people love your lewd material, so...*');
         message.channel.send('*React to this message to let me know if you want access to a totally not shady, but lewd area back behind her house...*');
     }
+    if (message.content === 'touhounaughtnaughtnaughty') {
+        message.channel.send('This is where I keep all of my Touhou ero material go... Feel free to add your own into my collection for others to see');
+        message.channel.send('Do not talk about anything illegal or contentious, even I am a stickler for that');
+    }
+    if (message.content === 'naughtytalkkoakumabeebee') {
+        message.channel.send('You can have all sorts of naughty conversations here');
+        message.channel.send('Do not talk about anything illegal or contentious, even I am a stickler for that');
+    }
     //if (message.content.startsWith("im")) {
    // message.channel.send("Hey Dad-bot, please shut up!");
   //  }
