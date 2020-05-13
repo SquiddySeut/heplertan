043e71581112e576cd496f4f3cdb0c512bdde3a5
SquiddySeut <40527468+SquiddySeut@users.noreply.github.com>
2020-05-12 21:07:54 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ client.on('message', message => {
     }
     if (message.content === 'pinyourfirstmessage') {
         channel.fetchMessages({limit: 1}).then(result => {
-            .then(message.pin());
+            message.pin();
         });
     }
     //if (message.content.startsWith("im")) {
